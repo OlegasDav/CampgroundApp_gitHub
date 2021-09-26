8f3150e33b2202deaaf6a8c2838e4c780f0ef800
Oleg <oldav0722@gmail.com>
2021-09-26 12:44:08 +0300

    Created repositories

---
+++ Persistence/Repositories/ICampgroundRepository.cs
@@ -0,0 +1,34 @@
+﻿using Persistence.Models.ReadModels;
+using Persistence.Models.WriteModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories
+{
+    public interface ICampgroundRepository
+    {
+        Task<IEnumerable<CampgroundReadModel>> GetAllAsync();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<CampgroundReadModel> GetAsync(Guid id);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        Task<CampgroundReadModel> GetAsync(Guid id, Guid userId);
+
+        Task<int> SaveOrUpdateAsync(CampgroundWriteModel campground);
+
+        Task<int> DeleteAsync(Guid id);
+    }
+}

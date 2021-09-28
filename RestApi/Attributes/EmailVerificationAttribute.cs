77d828dd2e62b406f190e4c4c932ce70e7f17fae
Oleg <oldav0722@gmail.com>
2021-09-28 21:53:50 +0300

    Added new attribute Email Verification

---
+++ RestApi/Attributes/EmailVerificationAttribute.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApi.Attributes
+{
+    public class EmailVerificationAttribute : Attribute, IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var isVerifiedEmail = bool.Parse(context.HttpContext.User.Claims.SingleOrDefault(claim => claim.Type == "email_verified").Value);
+
+            if (!isVerifiedEmail)
+            {
+                context.Result = new UnauthorizedObjectResult($"Your email is not verified!");
+
+                return;
+            }
+
+            await next();
+        }
+    }
+}
